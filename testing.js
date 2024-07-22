f255c331e0381dd937d150192cc746ca79bbdd4a
Dimitri <dima3tsotsoanva@gamil.comgit config --global user.name Dimitri1993>
2024-07-22 20:29:01 +0400

    davaleba JS

---
+++ testing.js
@@ -0,0 +1,47 @@
+/*let  x=85, y=65
+console.log(x+y)*/
+
+
+
+/*let student= {
+    firstname:"Dimitri",
+    lastname:"Tsotsonava",
+    age: 30,
+    email:"dima3tsotsonava@gmail.com",
+    marks:"123"
+}
+console.log(student.firstname)*/
+
+
+
+/*let m=45,n=23
+    if (m>n){
+        console.log(m+n)}
+            else {console.log(b)
+        }*/
+
+
+/*et studentName="dimitri"
+console.log("Rise and shine",studentName)*/
+
+
+
+
+/*let Bonus, salery=50
+    if (salery>=2000){
+        Bonus=0
+    }
+    else {Bonus=salery/10}
+    console.log(Bonus)*/
+
+
+
+
+    /*let a=23, b=a/2, c=a+b
+    console.log(c)*/
+
+
+
+
+    /*let Phones=['Iphone', 'Samsung', 'Pixel', 'Huawei', 'Xiaomi']
+    console.log(Phones)*/
